69056e82ed4aa4889cfc6d3b2a1e02c3eed1eedb
3x3x3 <ivmivm001@gmail.com>
2020-01-21 08:42:45 +0900

    new

---
+++ main.js
@@ -0,0 +1,98 @@
+var activeTabBtn = 0;
+var tabBtns = [];
+window.onload = function () {
+    var tabBtnsWrapper = this.document.getElementById("top_btn_wrapper");
+    tabBtns = tabBtnsWrapper.getElementsByTagName("input");
+    activeTabBtn = tabBtns[0];
+    var contentsWrappers = this.document.getElementsByClassName("contents_wrapper");
+    var _loop_1 = function (i) {
+        tabBtns[i].addEventListener("click", function () {
+            activeTabBtn = tabBtns[i];
+            for (var j = 0; j < contentsWrappers.length; j++) {
+                if (i === j) {
+                    contentsWrappers[j].style.display = "block";
+                }
+                else {
+                    contentsWrappers[j].style.display = "none";
+                }
+            }
+        });
+    };
+    for (var i = 0; i < tabBtns.length; i++) {
+        _loop_1(i);
+    }
+    var calcBtn = this.document.getElementById("calc_btn");
+    calcBtn.addEventListener("click", function () {
+        if (tabBtns[0] === activeTabBtn) {
+            calcInterpolation();
+        }
+    });
+};
+function calcInterpolation() {
+    var xInputs = [
+        document.getElementById("interp_x1"),
+        document.getElementById("interp_x2"),
+        document.getElementById("interp_x3")
+    ];
+    var yInputs = [
+        document.getElementById("interp_y1"),
+        document.getElementById("interp_y2"),
+        document.getElementById("interp_y3")
+    ];
+    var inputs = [];
+    var emptyCntX = 0;
+    var emptyCntY = 0;
+    var xVals = [];
+    var yVals = [];
+    for (var _i = 0, xInputs_1 = xInputs; _i < xInputs_1.length; _i++) {
+        var intp_in = xInputs_1[_i];
+        if (0 !== intp_in.value.length) {
+            xVals.push(Number(intp_in.value));
+        }
+        else {
+            xVals.push(null);
+            emptyCntX++;
+        }
+        inputs.push(intp_in);
+    }
+    for (var _a = 0, yInputs_1 = yInputs; _a < yInputs_1.length; _a++) {
+        var intp_in = yInputs_1[_a];
+        if (0 !== intp_in.value.length) {
+            yVals.push(Number(intp_in.value));
+        }
+        else {
+            yVals.push(null);
+            emptyCntY++;
+        }
+        inputs.push(intp_in);
+    }
+    if (1 !== (emptyCntX + emptyCntY)) {
+        return;
+    }
+    for (var i = 0; i < 2; i++) {
+        if (null === xVals[i] || null == yVals[i]) {
+            var bufferX = xVals[i];
+            var bufferY = yVals[i];
+            xVals[i] = xVals[i + 1];
+            yVals[i] = yVals[i + 1];
+            xVals[i + 1] = bufferX;
+            yVals[i + 1] = bufferY;
+        }
+    }
+    // 비어있는게 X라면 Y랑 바꿔서 계산하자
+    if (1 === emptyCntX) {
+        var buffer = xVals;
+        xVals = yVals;
+        yVals = buffer;
+    }
+    if (null === yVals[2]) {
+        yVals[2] = yVals[0] + (yVals[1] - yVals[0]) * (xVals[2] - xVals[0]) / (xVals[1] - xVals[0]);
+        for (var _b = 0, inputs_1 = inputs; _b < inputs_1.length; _b++) {
+            var intp_in = inputs_1[_b];
+            if (0 === intp_in.value.length) {
+                intp_in.value = yVals[2];
+                break;
+            }
+        }
+    }
+}
---
+++ main.ts
@@ -0,0 +1,110 @@
+let activeTabBtn = 0;
+let tabBtns: any = [];
+
+window.onload = function() {
+	const tabBtnsWrapper: HTMLDivElement = <HTMLDivElement>this.document.getElementById("top_btn_wrapper");
+	tabBtns = tabBtnsWrapper.getElementsByTagName("input");
+	activeTabBtn = tabBtns[0];
+
+	const contentsWrappers: any = this.document.getElementsByClassName("contents_wrapper");
+
+	for ( let i=0 ; i<tabBtns.length ; i++ ) {
+		tabBtns[i].addEventListener("click", function () {
+			activeTabBtn = tabBtns[i];
+
+			for ( let j=0 ; j<contentsWrappers.length ; j++ ) {
+				if ( i === j ) {
+					contentsWrappers[j].style.display = "block";
+				}
+				else {
+					contentsWrappers[j].style.display = "none";
+				}
+			}
+		});
+	}
+
+	const calcBtn: HTMLInputElement = <HTMLInputElement>this.document.getElementById("calc_btn");
+
+	calcBtn.addEventListener("click", function () {
+		if ( tabBtns[0] === activeTabBtn ) {
+			calcInterpolation();
+		}
+	});
+}
+
+function calcInterpolation() {
+	const xInputs: HTMLInputElement[] = [
+		<HTMLInputElement>document.getElementById("interp_x1"),
+		<HTMLInputElement>document.getElementById("interp_x2"),
+		<HTMLInputElement>document.getElementById("interp_x3")
+	]
+
+	const yInputs: HTMLInputElement[] = [
+		<HTMLInputElement>document.getElementById("interp_y1"),
+		<HTMLInputElement>document.getElementById("interp_y2"),
+		<HTMLInputElement>document.getElementById("interp_y3")
+	]
+
+	let inputs: HTMLInputElement[] = [];
+	let emptyCntX: number = 0;
+	let emptyCntY: number = 0;
+	let xVals: any[] = [];
+	let yVals: any[] = [];
+
+	for ( let intp_in of xInputs ) {
+		if ( 0 !== intp_in.value.length ) {
+			xVals.push(Number(intp_in.value));
+		}
+		else {
+			xVals.push(null);
+			emptyCntX++;
+		}
+
+		inputs.push(intp_in);
+	}
+
+	for ( let intp_in of yInputs ) {
+		if ( 0 !== intp_in.value.length ) {
+			yVals.push(Number(intp_in.value));
+		}
+		else {
+			yVals.push(null);
+			emptyCntY++;
+		}
+
+		inputs.push(intp_in);
+	}
+
+	if ( 1 !== (emptyCntX + emptyCntY) ) {
+		return;
+	}
+	
+	for ( let i=0 ; i<2 ; i++ ) {
+		if ( null === xVals[i] || null == yVals[i] ) {
+			let bufferX: any = xVals[i];
+			let bufferY: any = yVals[i];
+			xVals[i] = xVals[i+1];
+			yVals[i] = yVals[i+1];
+			xVals[i+1] = bufferX;
+			yVals[i+1] = bufferY;
+		}
+	}
+
+	// 비어있는게 X라면 Y랑 바꿔서 계산하자
+	if ( 1 === emptyCntX ) {
+		let buffer = xVals;
+		xVals = yVals;
+		yVals = buffer;
+	}
+
+	if ( null === yVals[2] ) {
+		yVals[2] = yVals[0] + (yVals[1] - yVals[0]) * (xVals[2] - xVals[0]) / (xVals[1] - xVals[0]);
+
+		for ( let intp_in of inputs ) {
+			if ( 0 === intp_in.value.length ) {
+				intp_in.value = yVals[2];
+				break;
+			}
+		}
+	}
+}	
