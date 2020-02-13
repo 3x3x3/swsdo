f49101d435f774dc6509210b2f1f016da165a061
3x3x3 <ivmivm001@gmail.com>
2020-02-13 15:42:04 +0900

    ver 1.0 작업 완료

--- main.js
+++ main.js
@@ -36,6 +36,12 @@ window.onload = function () {
             case tabBtns[2]:
                 calcAngle();
                 break;
+            case tabBtns[3]:
+                calcTmToLl();
+                break;
+            case tabBtns[4]:
+                calcLlToTm();
+                break;
             default:
                 break;
         }
@@ -189,10 +195,123 @@ function calcAngle() {
     calcAngleRet2.textContent = atlong(ret2);
 }
 function atlong(x) {
-    var data = Math.round(x * 10000);
     var dosu = Math.floor(x);
-    var min_decimal = data % 10000 * 60 / 10000;
-    var min = Math.floor(min_decimal);
-    var sec = Math.floor(min_decimal * 10000 % 10000 * 60 / 10000);
+    var min = Math.floor((x - Math.floor(x)) * 60);
+    var sec = (((x - Math.floor(x)) * 60 - Math.floor((x - Math.floor(x)) * 60)) * 60).toFixed(2);
     return dosu + "도 " + min + "분 " + sec + "초";
+}
+function calcTmToLl() {
+    var inEllipse = document.getElementById("tm_to_ll_ellipse").value;
+    var inFixTime = document.getElementById("tm_to_ll_fix_sec").checked;
+    var inOrigin = document.getElementById("tm_to_ll_origin").value;
+    var inOriginAdd = document.getElementById("tm_to_ll_origin_add").value;
+    var inDecimalStr = document.getElementById("tm_to_ll_decimal").value;
+    var inStrX = document.getElementById("tm_to_ll_x").value;
+    var inStrY = document.getElementById("tm_to_ll_y").value;
+    if (0 === inStrX.length || 0 === inStrY.length) {
+        return;
+    }
+    var inX = Number(inStrX);
+    var inY = Number(inStrY);
+    var inDecimal = Number(inDecimalStr);
+    var a = ("bassel" === inEllipse) ? 6377397.155 : 6378137; // 장반경
+    var f = ("bassel" === inEllipse) ? 1 / 299.1528128 : 1 / 298.257222101; // 편평률
+    var b = a * (1 - f);
+    var k0 = 1;
+    var delta_x = ("after_200912" === inOriginAdd) ? 600000 : (("jeju" === inOrigin) ? 550000 : 500000);
+    var delta_y = 200000;
+    var pi0 = 38;
+    var pi0_rad = pi0 / 180 * Math.PI;
+    var lambda0 = 0;
+    if ("west" === inOrigin) {
+        lambda0 = 125;
+    }
+    else if ("east" === inOrigin) {
+        lambda0 = 129;
+    }
+    else if ("east_sea" === inOrigin) {
+        lambda0 = 131;
+    }
+    else {
+        lambda0 = 127;
+    }
+    var lambda0_rad = lambda0 / 180 * Math.PI;
+    var e_sq = ((a * a) - (b * b)) / (a * a);
+    var ep_sq = ((a * a) - (b * b)) / (b * b);
+    var m0 = a * ((1 - e_sq / 4 - 3 * e_sq * e_sq / 64 - 5 * e_sq * e_sq * e_sq / 256) * pi0_rad - (3 * e_sq / 8 + 3 * e_sq * e_sq / 32 + 45 * e_sq * e_sq * e_sq / 1024) * Math.sin(2 * pi0_rad) + (15 * e_sq * e_sq / 256 + 45 * e_sq * e_sq * e_sq / 1024) * Math.sin(4 * pi0_rad) - (35 * e_sq * e_sq * e_sq / 3072) * Math.sin(6 * pi0_rad));
+    var e1 = (1 - Math.sqrt(1 - e_sq)) / (1 + Math.sqrt(1 - e_sq));
+    var time_fix = inFixTime ? 10.405 / 3600 : 0;
+    var m = m0 + ((inX - delta_x) / k0);
+    var mu1 = m / (a * (1 - e_sq / 4 - 3 * e_sq * e_sq / 64 - 5 * e_sq * e_sq * e_sq / 256));
+    var pi1 = mu1 + (3 * e1 / 2 - 27 * e1 * e1 * e1 / 32) * Math.sin(2 * mu1) + (21 * e1 * e1 / 16 - 55 * e1 * e1 * e1 * e1 / 32) * Math.sin(4 * mu1) + (151 * e1 * e1 * e1 / 96) * Math.sin(6 * mu1) + (1097 * e1 * e1 * e1 * e1 / 512) * Math.sin(8 * mu1);
+    var r1 = (a * (1 - e_sq)) / Math.pow(1 - e_sq * Math.sin(pi1) * Math.sin(pi1), 3 / 2);
+    var c1 = ep_sq * Math.cos(pi1) * Math.cos(pi1);
+    var t1 = Math.tan(pi1) * Math.tan(pi1);
+    var n1 = a / Math.sqrt(1 - e_sq * Math.sin(pi1) * Math.sin(pi1));
+    var d = (inY - delta_y) / (n1 * k0);
+    var pi = (pi1 - (n1 * Math.tan(pi1) / r1) * (d * d / 2 - d * d * d * d / 24 * (5 + 3 * t1 + 10 * c1 - 4 * c1 * c1 - 9 * ep_sq) + d * d * d * d * d * d / 720 * (61 + 90 * t1 + 298 * c1 + 45 * t1 * t1 - 252 * ep_sq - 3 * c1 * c1))) * 180 / Math.PI;
+    var lambda = lambda0 + ((1 / Math.cos(pi1)) * (d - (d * d * d / 6) * (1 + 2 * t1 + c1) + (d * d * d * d * d / 120) * (5 - 2 * c1 + 28 * t1 - 3 * c1 * c1 + 8 * ep_sq + 24 * t1 * t1))) * 180 / Math.PI + time_fix;
+    var longitude1 = document.getElementById("tm_to_ll_longitude_1");
+    var longitude2 = document.getElementById("tm_to_ll_longitude_2");
+    var latitude1 = document.getElementById("tm_to_ll_latitude_1");
+    var latitude2 = document.getElementById("tm_to_ll_latitude_2");
+    longitude1.textContent = lambda.toFixed(inDecimal).toString();
+    longitude2.textContent = atlong(lambda);
+    latitude1.textContent = pi.toFixed(inDecimal).toString();
+    latitude2.textContent = atlong(pi);
+}
+function calcLlToTm() {
+    var inEllipse = document.getElementById("ll_to_tm_ellipse").value;
+    var inFixTime = document.getElementById("ll_to_tm_fix_sec").checked;
+    var inOrigin = document.getElementById("ll_to_tm_origin").value;
+    var inOriginAdd = document.getElementById("ll_to_tm_origin_add").value;
+    var inDecimalStr = document.getElementById("ll_to_tm_decimal").value;
+    var inStrLambda = document.getElementById("ll_to_tm_longitude").value;
+    var inStrPi = document.getElementById("ll_to_tm_latitude").value;
+    if (0 === inStrLambda.length || 0 === inStrPi.length) {
+        return;
+    }
+    var inLambda = Number(inStrLambda);
+    var inPi = Number(inStrPi);
+    var inDecimal = Number(inDecimalStr);
+    var a = ("bassel" === inEllipse) ? 6377397.155 : 6378137; // 장반경
+    var f = ("bassel" === inEllipse) ? 1 / 299.1528128 : 1 / 298.257222101; // 편평률
+    var b = a * (1 - f);
+    var k0 = 1;
+    var delta_x = ("after_200912" === inOriginAdd) ? 600000 : (("jeju" === inOrigin) ? 550000 : 500000);
+    var delta_y = 200000;
+    var pi0 = 38;
+    var pi0_rad = pi0 / 180 * Math.PI;
+    var lambda0 = 0;
+    if ("west" === inOrigin) {
+        lambda0 = 125;
+    }
+    else if ("east" === inOrigin) {
+        lambda0 = 129;
+    }
+    else if ("east_sea" === inOrigin) {
+        lambda0 = 131;
+    }
+    else {
+        lambda0 = 127;
+    }
+    var lambda0_rad = lambda0 / 180 * Math.PI;
+    var e_sq = ((a * a) - (b * b)) / (a * a);
+    var ep_sq = ((a * a) - (b * b)) / (b * b);
+    var m0 = a * ((1 - e_sq / 4 - 3 * e_sq * e_sq / 64 - 5 * e_sq * e_sq * e_sq / 256) * pi0_rad - (3 * e_sq / 8 + 3 * e_sq * e_sq / 32 + 45 * e_sq * e_sq * e_sq / 1024) * Math.sin(2 * pi0_rad) + (15 * e_sq * e_sq / 256 + 45 * e_sq * e_sq * e_sq / 1024) * Math.sin(4 * pi0_rad) - (35 * e_sq * e_sq * e_sq / 3072) * Math.sin(6 * pi0_rad));
+    var e1 = (1 - Math.sqrt(1 - e_sq)) / (1 + Math.sqrt(1 - e_sq));
+    var time_fix = inFixTime ? 10.405 / 3600 : 0;
+    var pi = inPi / 180 * Math.PI;
+    var lambda = (inLambda - time_fix) / 180 * Math.PI;
+    var t = Math.tan(pi) * Math.tan(pi);
+    var c = (e_sq / (1 - e_sq)) * Math.cos(pi) * Math.cos(pi);
+    var l_a = (lambda - lambda0_rad) * Math.cos(pi);
+    var n = a / Math.sqrt(1 - e_sq * Math.sin(pi) * Math.sin(pi));
+    var m = a * ((1 - e_sq / 4 - 3 * e_sq * e_sq / 64 - 5 * e_sq * e_sq * e_sq / 256) * pi - (3 * e_sq / 8 + 3 * e_sq * e_sq / 32 + 45 * e_sq * e_sq * e_sq / 1024) * Math.sin(2 * pi) + (15 * e_sq * e_sq / 256 + 45 * e_sq * e_sq * e_sq / 1024) * Math.sin(4 * pi) - 35 * e_sq * e_sq * e_sq / 3072 * Math.sin(6 * pi));
+    var x = delta_x + k0 * (m - m0 + n * Math.tan(pi) * (l_a * l_a / 2 + l_a * l_a * l_a * l_a / 24 * (5 - t + 9 * c + 4 * c * c) + l_a * l_a * l_a * l_a * l_a * l_a / 720 * (61 - 58 * t + t * t + 600 * c - 330 * e_sq)));
+    var y = delta_y + k0 * n * (l_a + l_a * l_a * l_a / 6 * (1 - t + c) + l_a * l_a * l_a * l_a * l_a / 120 * (5 - 18 * t + t * t + 72 * c - 58 * ep_sq));
+    var outX = document.getElementById("ll_to_tm_x");
+    var outY = document.getElementById("ll_to_tm_y");
+    outX.textContent = x.toFixed(inDecimal).toString();
+    outY.textContent = y.toFixed(inDecimal).toString();
 }
--- main.ts
+++ main.ts
@@ -42,6 +42,14 @@ window.onload = function() {
 				calcAngle();
 				break;
 
+			case tabBtns[3]:
+				calcTmToLl();
+				break;
+			
+			case tabBtns[4]:
+				calcLlToTm();
+				break;
+
 			default:
 				break;
 		}
@@ -219,11 +227,148 @@ function calcAngle() {
 }
 
 function atlong(x: number) {
-	const data = Math.round(x * 10000);
 	const dosu = Math.floor(x);
-	const min_decimal = data % 10000 * 60 / 10000;
-	const min = Math.floor(min_decimal);
-	const sec = Math.floor(min_decimal * 10000 % 10000 * 60 / 10000);
+	const min = Math.floor((x - Math.floor(x)) * 60);
+	const sec = (((x - Math.floor(x)) * 60 - Math.floor((x-Math.floor(x)) * 60)) * 60).toFixed(2);
 
 	return dosu + "도 " + min + "분 " + sec + "초";
+}
+
+function calcTmToLl() {
+	const inEllipse: string = (<HTMLInputElement>document.getElementById("tm_to_ll_ellipse")).value;
+	const inFixTime: boolean = (<HTMLInputElement>document.getElementById("tm_to_ll_fix_sec")).checked;
+	const inOrigin: string = (<HTMLInputElement>document.getElementById("tm_to_ll_origin")).value;
+	const inOriginAdd: string = (<HTMLInputElement>document.getElementById("tm_to_ll_origin_add")).value;
+	const inDecimalStr: string = (<HTMLInputElement>document.getElementById("tm_to_ll_decimal")).value;
+
+	const inStrX: string = (<HTMLInputElement>document.getElementById("tm_to_ll_x")).value;
+	const inStrY: string = (<HTMLInputElement>document.getElementById("tm_to_ll_y")).value;
+
+	if ( 0 === inStrX.length || 0 === inStrY.length ) {
+		return;
+	}
+
+	const inX: number = Number(inStrX);
+	const inY: number = Number(inStrY);
+	const inDecimal: number = Number(inDecimalStr);
+
+	const a: number = ("bassel" === inEllipse) ? 6377397.155 : 6378137;	// 장반경
+	const f: number = ("bassel" === inEllipse) ? 1 / 299.1528128 : 1 / 298.257222101;	// 편평률
+	const b: number = a * (1-f);
+	const k0: number = 1;
+	const delta_x: number = ("after_200912" === inOriginAdd) ? 600000 : ( ("jeju" === inOrigin) ? 550000 : 500000);
+	const delta_y: number = 200000;
+	const pi0: number = 38;
+	const pi0_rad: number = pi0 / 180 * Math.PI;
+	let lambda0: number = 0;
+
+	if ( "west" === inOrigin ) {
+		lambda0 = 125;
+	}
+	else if ( "east" === inOrigin ) {
+		lambda0 = 129;
+	}
+	else if ( "east_sea" === inOrigin ) {
+		lambda0 = 131;
+	}
+	else {
+		lambda0 = 127;
+	}
+
+	const lambda0_rad = lambda0 / 180 * Math.PI;
+
+	const e_sq: number = ((a * a) - (b * b)) / (a * a);
+	const ep_sq: number = ((a * a) - (b * b)) / (b * b);
+	const m0: number = a * ((1-e_sq/4-3*e_sq*e_sq/64-5*e_sq*e_sq*e_sq/256)*pi0_rad-(3*e_sq/8+3*e_sq*e_sq/32+45*e_sq*e_sq*e_sq/1024)*Math.sin(2*pi0_rad)+(15*e_sq*e_sq/256+45*e_sq*e_sq*e_sq/1024)*Math.sin(4*pi0_rad)-(35*e_sq*e_sq*e_sq/3072)*Math.sin(6*pi0_rad));
+	const e1: number = (1 - Math.sqrt(1-e_sq)) / (1 + Math.sqrt(1-e_sq));
+	const time_fix: number = inFixTime ? 10.405/3600 : 0;
+
+	const m: number = m0 + ((inX-delta_x)/k0);
+	const mu1: number = m / (a*(1-e_sq/4-3*e_sq*e_sq/64-5*e_sq*e_sq*e_sq/256));
+	const pi1: number = mu1 + (3*e1/2-27*e1*e1*e1/32) * Math.sin(2*mu1) + (21*e1*e1/16-55*e1*e1*e1*e1/32) * Math.sin(4*mu1) + (151*e1*e1*e1/96) * Math.sin(6*mu1) + (1097*e1*e1*e1*e1/512) * Math.sin(8*mu1);
+	const r1: number = (a*(1-e_sq)) / Math.pow(1-e_sq*Math.sin(pi1)*Math.sin(pi1), 3/2);
+	const c1: number = ep_sq * Math.cos(pi1) * Math.cos(pi1);
+	const t1: number = Math.tan(pi1) * Math.tan(pi1);
+	const n1: number = a / Math.sqrt(1-e_sq*Math.sin(pi1)*Math.sin(pi1));
+	const d: number = (inY-delta_y) / (n1*k0);
+
+	const pi = (pi1-(n1*Math.tan(pi1)/r1)*(d*d/2-d*d*d*d/24*(5+3*t1+10*c1-4*c1*c1-9*ep_sq)+d*d*d*d*d*d/720*(61+90*t1+298*c1+45*t1*t1-252*ep_sq-3*c1*c1))) * 180/Math.PI;
+	const lambda = lambda0 + ((1/Math.cos(pi1))*(d-(d*d*d/6)*(1+2*t1+c1)+(d*d*d*d*d/120)*(5-2*c1+28*t1-3*c1*c1+8*ep_sq+24*t1*t1))) * 180/Math.PI + time_fix;
+
+	const longitude1: HTMLElement = <HTMLElement>document.getElementById("tm_to_ll_longitude_1");
+	const longitude2: HTMLElement = <HTMLElement>document.getElementById("tm_to_ll_longitude_2");
+	const latitude1: HTMLElement = <HTMLElement>document.getElementById("tm_to_ll_latitude_1");
+	const latitude2: HTMLElement = <HTMLElement>document.getElementById("tm_to_ll_latitude_2");
+	
+	longitude1.textContent = lambda.toFixed(inDecimal).toString();
+	longitude2.textContent = atlong(lambda);
+	latitude1.textContent = pi.toFixed(inDecimal).toString();
+	latitude2.textContent = atlong(pi);
+}
+
+function calcLlToTm() {
+	const inEllipse: string = (<HTMLInputElement>document.getElementById("ll_to_tm_ellipse")).value;
+	const inFixTime: boolean = (<HTMLInputElement>document.getElementById("ll_to_tm_fix_sec")).checked;
+	const inOrigin: string = (<HTMLInputElement>document.getElementById("ll_to_tm_origin")).value;
+	const inOriginAdd: string = (<HTMLInputElement>document.getElementById("ll_to_tm_origin_add")).value;
+	const inDecimalStr: string = (<HTMLInputElement>document.getElementById("ll_to_tm_decimal")).value;
+	
+	const inStrLambda: string = (<HTMLInputElement>document.getElementById("ll_to_tm_longitude")).value;
+	const inStrPi: string = (<HTMLInputElement>document.getElementById("ll_to_tm_latitude")).value;
+	
+	if ( 0 === inStrLambda.length || 0 === inStrPi.length ) {
+		return;
+	}
+	
+	const inLambda: number = Number(inStrLambda);
+	const inPi: number = Number(inStrPi);
+	const inDecimal: number = Number(inDecimalStr);
+	
+	const a: number = ("bassel" === inEllipse) ? 6377397.155 : 6378137;	// 장반경
+	const f: number = ("bassel" === inEllipse) ? 1 / 299.1528128 : 1 / 298.257222101;	// 편평률
+	const b: number = a * (1-f);
+	const k0: number = 1;
+	const delta_x: number = ("after_200912" === inOriginAdd) ? 600000 : ( ("jeju" === inOrigin) ? 550000 : 500000);
+	const delta_y: number = 200000;
+	const pi0: number = 38;
+	const pi0_rad: number = pi0 / 180 * Math.PI;
+	let lambda0: number = 0;
+	
+	if ( "west" === inOrigin ) {
+		lambda0 = 125;
+	}
+	else if ( "east" === inOrigin ) {
+		lambda0 = 129;
+	}
+	else if ( "east_sea" === inOrigin ) {
+		lambda0 = 131;
+	}
+	else {
+		lambda0 = 127;
+	}
+	
+	const lambda0_rad = lambda0 / 180 * Math.PI;
+	
+	const e_sq: number = ((a * a) - (b * b)) / (a * a);
+	const ep_sq: number = ((a * a) - (b * b)) / (b * b);
+	const m0: number = a * ((1-e_sq/4-3*e_sq*e_sq/64-5*e_sq*e_sq*e_sq/256)*pi0_rad-(3*e_sq/8+3*e_sq*e_sq/32+45*e_sq*e_sq*e_sq/1024)*Math.sin(2*pi0_rad)+(15*e_sq*e_sq/256+45*e_sq*e_sq*e_sq/1024)*Math.sin(4*pi0_rad)-(35*e_sq*e_sq*e_sq/3072)*Math.sin(6*pi0_rad));
+	const e1: number = (1 - Math.sqrt(1-e_sq)) / (1 + Math.sqrt(1-e_sq));
+	const time_fix: number = inFixTime ? 10.405/3600 : 0;
+
+	const pi: number = inPi / 180 * Math.PI;
+	const lambda: number = (inLambda-time_fix) / 180 * Math.PI;
+	const t: number = Math.tan(pi) * Math.tan(pi);
+	const c: number = (e_sq / (1-e_sq)) * Math.cos(pi) * Math.cos(pi);
+	const l_a: number = (lambda-lambda0_rad) * Math.cos(pi);
+	const n: number = a / Math.sqrt(1-e_sq*Math.sin(pi)*Math.sin(pi));
+	const m: number = a * ((1-e_sq/4-3*e_sq*e_sq/64-5*e_sq*e_sq*e_sq/256)*pi-(3*e_sq/8+3*e_sq*e_sq/32+45*e_sq*e_sq*e_sq/1024)*Math.sin(2*pi)+(15*e_sq*e_sq/256+45*e_sq*e_sq*e_sq/1024)*Math.sin(4*pi)-35*e_sq*e_sq*e_sq/3072*Math.sin(6*pi));
+
+	const x: number = delta_x + k0 * (m-m0+n*Math.tan(pi)*(l_a*l_a/2 + l_a*l_a*l_a*l_a/24 * (5-t+9*c+4*c*c) + l_a*l_a*l_a*l_a*l_a*l_a/720 * (61-58*t+t*t+600*c-330*e_sq)));
+	const y: number = delta_y + k0 * n * (l_a + l_a*l_a*l_a/6 * (1-t+c) + l_a*l_a*l_a*l_a*l_a/120 * (5-18*t+t*t+72*c-58 * ep_sq));
+
+	const outX: HTMLElement = <HTMLElement>document.getElementById("ll_to_tm_x");
+	const outY: HTMLElement = <HTMLElement>document.getElementById("ll_to_tm_y");
+
+	outX.textContent = x.toFixed(inDecimal).toString();
+	outY.textContent = y.toFixed(inDecimal).toString();
 }
